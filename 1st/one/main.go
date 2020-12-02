7e445c6a0f156484ccf3b67542c4055a6079bc19
George Webb <george.webb@voidsource.com>
2020-12-02 10:45:43 +0000

    day 1

---
+++ 1st/one/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	f, err := os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	numbers := make([]int, 0)
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			continue
+		}
+		n, err := strconv.Atoi(line)
+		if err != nil {
+			panic(err)
+		}
+		numbers = append(numbers, n)
+	}
+
+	err = scanner.Err()
+	if err != nil {
+		panic(err)
+	}
+
+	for i := 0; i < len(numbers); i++ {
+		for j := i+1; j < len(numbers); j++ {
+			if numbers[i] + numbers[j] == 2020 {
+				fmt.Println(numbers[i] * numbers[j])
+				return
+			}
+		}
+	}
+	fmt.Println("nope")
+}
---
+++ 1st/two/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	f, err := os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	numbers := make([]int, 0)
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			continue
+		}
+		n, err := strconv.Atoi(line)
+		if err != nil {
+			panic(err)
+		}
+		numbers = append(numbers, n)
+	}
+
+	err = scanner.Err()
+	if err != nil {
+		panic(err)
+	}
+
+	for i := 0; i < len(numbers); i++ {
+		for j := i+1; j < len(numbers); j++ {
+			for k := j+1; k < len(numbers); k++ {
+				if numbers[i] + numbers[j] + numbers[k] == 2020 {
+					fmt.Println(numbers[i] * numbers[j] * numbers[k])
+					return
+				}
+			}
+		}
+	}
+	fmt.Println("nope")
+}
