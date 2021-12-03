ccbdcb5edd4904bfd2a9f615db476e2f6f2fb509
George Webb <george.webb@voidsource.com>
2021-12-03 09:05:10 +0000

    day 2

---
+++ 2021/02/pt1/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	f, err := os.Open("2021/02/pt1/input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	x := 0
+	z := 0
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			continue
+		}
+		words := strings.Split(line, " ")
+		if len(words) != 2 {
+			panic("invalid instruction")
+		}
+		inst := words[0]
+		n, err := strconv.Atoi(words[1])
+		if err != nil {
+			panic(err)
+		}
+
+		switch inst {
+		case "forward":
+			x += n
+		case "up":
+			z -= n
+		case "down":
+			z += n
+		}
+	}
+
+	err = scanner.Err()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("X: %d, Z: %d, product: %d", x, z, z*x)
+}
